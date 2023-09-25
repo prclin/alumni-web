f39b2fe64d93546dd0f99fbe30382e2728158ab1
imyttizy <1664619620@qq.com>
2023-09-25 15:02:13 +0800

    feat: finish basic message view

--- src/views/MessageView/index.tsx
+++ src/views/MessageView/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Avatar, Space } from 'antd'
+
+const MessageCard: React.FC = () => {
+  return (
+    <Space className="cursor-pointer">
+      <Avatar size={40} src="/avatar.svg"/>
+      <div className="flex flex-col justify-evenly">
+        <span className="font-bold">nickname</span>
+        <span className="text-xs">last message</span>
+      </div>
+    </Space>
+  )
+}
 
 const MessageView: React.FC = () => (
-  <div>message</div>
+  <Space size="large" className="px-2 py-1" direction="vertical">
+    <MessageCard/>
+    <MessageCard/>
+    <MessageCard/>
+    <MessageCard/>
+    <MessageCard/>
+  </Space>
 )
 
 export default MessageView
